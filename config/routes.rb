c770390c6478ee8a7674b33f5ec9788836830bd1
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-05-12 14:45:01 +0200

    about page created

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :posts
   get 'pages/home'
   get 'actualites', to: 'pages#posts', as: :actualites
+  get 'a_propos', to: 'pages#about', as: :about
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
