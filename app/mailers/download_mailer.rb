839515808894209792c775ddd7e1c1372f579d6c
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-05-16 11:24:42 +0200

    MailGun config done v2

--- app/mailers/download_mailer.rb
+++ app/mailers/download_mailer.rb
@@ -2,6 +2,6 @@ class DownloadMailer < ApplicationMailer
   def download_request
     @post = params[:post]
     @email = params[:email]
-    mail(to: "p-edacquet@hotmail.fr", subject: 'Demande de téléchargement')
+    mail(to: "sylviemouchezcoach@gmail.com", cc: "p-edacquet@hotmail.fr", subject: 'Demande de téléchargement')
   end
 end
