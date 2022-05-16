e2ab85d6b5a4eb42e7779fdfee2b8a813bdb317d
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-05-16 10:35:18 +0200

    mailgun config done

--- app/mailers/download_mailer.rb
+++ app/mailers/download_mailer.rb
@@ -2,6 +2,6 @@ class DownloadMailer < ApplicationMailer
   def download_request
     @post = params[:post]
     @email = params[:email]
-    mail(to: "sylviemouchezcoach@gmail.com", subject: 'Demande de téléchargement')
+    mail(to: "sylviemouchezcoach@gmail.com", cc: "p-edacquet@hotmail.fr, philippe.nougaillon@gmail.com", subject: 'Demande de téléchargement')
   end
 end
