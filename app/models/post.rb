61dd51b6f873fd3d304a2b922ad5d79c9696b028
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-05-17 17:00:04 +0200

    Post / Reference : validates presence

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
+  validates :title, :content, :category, presence: true
   default_scope -> { order(updated_at: :desc) }
 end
--- app/models/reference.rb
+++ app/models/reference.rb
@@ -1,2 +1,3 @@
 class Reference < ApplicationRecord
+  validates :name, presence: true
 end
