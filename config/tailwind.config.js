313418e7c52e5cc19479ee6e6312d6b8b90add56
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-05-23 16:55:15 +0200

    daisyui : custom theme

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,6 +1,23 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        admouvoir: {
+          "primary": "#61d2dc",
+          "secondary": "#276c7f",
+          "accent": "#37CDBE",
+          "neutral": "#3D4451",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
   content: [
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
@@ -17,5 +34,6 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
+    require("daisyui")
   ]
 }
